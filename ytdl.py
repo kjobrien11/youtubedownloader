cd60de16fa35bd1989d7dc82873ff023f90b4e01
KJ O'BRIEN <kjokjo219@gmail.com>
2023-03-10 21:27:28 +0000

    Starts GUI Build

--- ytdl.py
+++ ytdl.py
@@ -3,6 +3,20 @@ import yt_dlp as youtube_dl
 import ffmpy
 import os
 import argparse
+import PySimpleGUI as sg
+
+
+layout = [
+      [sg.Text('Youtube Downloader')],
+      [sg.Text('Youtube URl:', size=(20, 1)), sg.InputText()],
+      [sg.Text('Output File Name:', size=(20, 1)), sg.InputText()],
+      [sg.Submit(), sg.Cancel()]]
+
+window = sg.Window('PictureSync', layout)
+
+event, values = window.read()
+window.close()
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--i', type=str, required=True)
